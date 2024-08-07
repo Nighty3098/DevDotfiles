c40d721a7b4ee5656abbdf525754fb99d4cde638
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-08-07 22:41:09 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cp -r scripts ~/
+
+mv ~/.config/conky ~/.config/conky_old
+mv ~/.config/htop ~/.config/htop_old
+mv ~/.config/i3 ~/.config/i3_old
+mv ~/.cofig/kitty ~/.config/kitty_old
+mv ~/.config/neofetch ~/.config/neofetch_old
+mv ~/.config/nvim ~/.config/nvim_old
+mv ~/.config/polybar ~/.config/polybar_old
+mv ~/.config/ranger ~/.config/ranger_old
+mv ~/.config/rofi ~/.config/rofi_old
+
+cd .config
+
+cp -r conky ~/.config/
+cp -r htop ~/.config/
+cp -r i3 ~/.config/
+cp -r kitty ~/.config/
+cp -r neofetch ~/.config/
+cp -r nvim ~/.config/
+cp -r polybar ~/.config/
+cp -r ranger ~/.config/
+cp -r rofi ~/.config/
+
+cp starship ~/.config/
+
+echo "Done!"
